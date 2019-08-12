46f972a66414338c227b2dd2ec54b1e06f5da330
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-12 12:43:08 -0700

    Agregar archivos de proyecto.

---
+++ ModificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cuenta
+    {
+        public float saldo { get; set; }
+        public Cliente cliente { get; set; }
+        private string _identificador;
+        public string Identificador
+        {
+            get
+            {
+                return _identificador;
+            }
+            set
+            {
+                if (value.Length ==10)
+                {
+                    _identificador = value;
+                }
+                else
+                {
+                    throw new Exception("Ocupa ser 10 caracteres");
+                }
+            }
+        }
+    }
+}
---
+++ ModificadoresDeAcceso/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nombre = "Jesus";
+            cliente.Telefono = "(644)4477007";
+
+            Console.WriteLine("Nombre: " + cliente.Nombre);
+            Console.WriteLine("Telélefono: " + cliente.Telefono);
+
+            try
+            {
+                cliente.Curp = "12345678912345679";
+                Console.WriteLine("Curp:" + cliente.Curp);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("La CURP debe de ser de 18 caracteres");
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
